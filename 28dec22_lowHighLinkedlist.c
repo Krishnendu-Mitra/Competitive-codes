8a0b986e288375cb06983cfda4d3caa9af320231
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2022-12-28 21:14:41 +0530

    New Update

--- 28dec22_lowHighLinkedlist.c
+++ 28dec22_lowHighLinkedlist.c
@@ -1,4 +1,10 @@
-
+//Question------------------------------------->
+/*Given a lined list, rearrange the node values such that appear
+in alternating 
+low -> high -> low -> high -> ...form.
+For example, give 1->2->3->4->5..., you should return
+1->3->2->5->4...*/
+//Answer--------------------------------------->
 #include<stdio.h>
 #include<stdlib.h>
 //code by krish
@@ -8,7 +14,7 @@ struct Node{
 };
 typedef struct Node node;
 node *head;
-node *createNode(int element){
+node *createNode(int element){//very importent function or this programe
 	node *temp;
 	temp = (node*)malloc(sizeof(node));
 	temp -> element = element;  
@@ -27,7 +33,7 @@ void push(node** head, int new_element){
     // cache->link=krish;
     // krish->link= NULL;
 }
-void display(node* head){
+void display(node* head,int n){
     node* temp = head;
     printf("<<< ");
     while(temp){
@@ -36,15 +42,15 @@ void display(node* head){
     }
     printf("\n");
 }
-void swap(node *a, node *b){
+void swap(node *a, node *b){//swap nodes only
     int temp = a -> element;
     a -> element = b -> element;
     b -> element = temp;
 }
-void calculation(node *head){
+void calculation(node *head){//main function were the real game done
     int cache = 0;
-    node* past = head;
-    node* present = head->link;
+    node* past = head; //past node
+    node* present = head->link; //present node
     if(head == NULL){
         return;
     }
@@ -52,18 +58,18 @@ void calculation(node *head){
         if(past->element > present->element){
             swap(past, present);
         }else{
-            cache++;
+            cache++; //importent but not not visible code
         }
         if(present->link && present->link->element > present->element){
             swap(present->link, present);
         }else{
-            cache++;
+            cache++; //only devloper use
         }
         past = present->link;
         if(!present->link){
             break;
         }else{
-            cache++;
+            cache++; //check the inner code work
         }
         present = present->link->link;
     }
@@ -71,19 +77,19 @@ void calculation(node *head){
 void play(){
     int i, temp, n;
     node *head = NULL;
-    printf("Enter the size of your list: ");
+    printf("Enter the size of your list: ");//leanth of my list
     scanf("%d",&n);
     printf("Please give the sorted  & revers list\n");
     for(i = 0; i <= n - 1; i++){
-        printf("%d no element = ",i+1);
+        printf("%d no element = ",i+1);//get the elements one by one
         scanf("%d",&temp);
         push(&head, temp);
-    }
+    }//user def list
     printf("Your given list is: \n");
-    display(head);
+    display(head,n);//throw the head and size
     calculation(head);
     printf("Result is: \n");
-    display(head);
+    display(head,n);
 }
 int main(){
     play();//only one function call here
