63279e19ef0d8988a58d628aad2dd96b78a659ed
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2023-01-09 21:28:54 +0530

    Incomplete Update

--- 5jan23_matrixWall.py
+++ 5jan23_matrixWall.py
@@ -1,14 +1,29 @@
 #code by krish
+v = []
+arr = []
 #functions---------------------------->
 def getdata(n):
     v1 = 0
     v2 = 0
     v3 = 0
     v4 = 1
+    j = 0
     for i in range(0,n,1):
-        v1 = int(input())
+        print("[",i+1,j+1,"]data: ")
+        v1 = int(input("<<< "))
+        print("[",i+1,j+2,"]data: ")
+        v2 = int(input("<<< "))
+        print("[",i+1,j+3,"]data: ")
+        v3 = int(input("<<< "))
+        print("[",i+1,j+4,"]data: ")
+        v4 = int(input("<<< "))
+        v = [v1,v2,v3,v4]
+        arr.append(v)
+        if(j >= 4):
+            j = 0
+    print("Your data is: ",arr)
 #main--------------------------------->
-n = int(input("Enter the size of list: "))
+n = int(input("Enter the step of matrix: "))
 if(n >= 0):
     print("Enter your data one by one\n")
     getdata(n)
